8b48ec830920417d1dab535f2307bb050b7eff69
konceptosociala <sanerhp.mail@gmail.com>
2023-11-16 13:26:04 +0200

    initial commit

---
+++ src/Ball.java
@@ -0,0 +1,16 @@
+import acm.graphics.GOval;
+
+public class Ball extends GOval {
+
+	public Ball(double x, double y, double width, double height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+/*
+
+* 
+
+*/
---
+++ src/Brick.java
@@ -0,0 +1,10 @@
+import acm.graphics.GRect;
+
+public class Brick extends GRect {
+
+	public Brick(double width, double height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
