58ff9c1533d1479c4be86dfa080ae83db6903e74
Relrige <91017454+Relrige@users.noreply.github.com>
2023-11-23 00:02:43 +0200

    Update Paddle.java

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,10 +1,34 @@
+import java.awt.Color;
+
+import acm.graphics.GCompound;
 import acm.graphics.GRect;
 
-public class Paddle extends GRect {
+public class Paddle extends GCompound {
+    private double X;
+    private double Y;
+    private double width;
+    private double trgX;
+    private GRect paddle;
 
-	public Paddle(double x, double y, double width, double height) {
-		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
-	}
+    // needed
+    public static int MouseX;
+    public static int MouseY;
 
+    public Paddle(double X, double Y, double width, double height) {
+        this.X = X;
+        this.Y = Y;
+        this.width = width;
+        this.trgX = X;
+        paddle = new GRect(X, Y, width, height);
+        paddle.setFilled(true);
+        add(paddle);
+        setLocation(0, Breakout.APPLICATION_HEIGHT-height-150);
+        setColor(Color.BLACK);
+    }
+
+    public void move() {
+
+        trgX = MouseX - width / 2;
+        paddle.setLocation(trgX, Y);
+    }
 }
