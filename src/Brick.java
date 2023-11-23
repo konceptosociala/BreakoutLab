0b0b00fce1eef737c96d56fddd85ce063218789a
luckasonic <151017044+luckasonic@users.noreply.github.com>
2023-11-23 11:47:32 +0200

    Update Brick.java

--- src/Brick.java
+++ src/Brick.java
@@ -1,10 +1,34 @@
 import acm.graphics.GRect;
 
+import acm.graphics.GRect;
+
+
+import java.awt.*;
+
 public class Brick extends GRect {
 
-	public Brick(double width, double height) {
-		super(width, height);
-		// TODO Auto-generated constructor stub
-	}
+	public Brick(double x, double y, double width, double height) {
+		super(x, y, width, height);
 
-}
+
+		if (Breakout.row <= 2) {
+			super.setColor(Color.RED);}
+
+		else if (Breakout.row <= 4) {
+			super.setColor(Color.ORANGE);
+		}
+		else if (Breakout.row <= 6) {
+			super.setColor(Color.YELLOW);
+		}
+		else if (Breakout.row <= 8) {
+			super.setColor(Color.GREEN);
+		}
+		else if (Breakout.row <= 10) {
+			super.setColor(Color.CYAN);
+		}
+		super.setFilled(true);
+
+		}
+
+
+	}
